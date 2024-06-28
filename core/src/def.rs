99105d2ff75dc443f4548e02597e4fa6b58e31cd
Bo Maryniuk <bo@maryniuk.net>
2024-06-28 20:35:31 +0200

    Add default constants

---
+++ core/src/def.rs
@@ -0,0 +1,23 @@
+/// Defaults and constants
+///
+
+// Main binary name
+pub const APLT_MAIN: &str = "alfad";
+
+// Ctl applet
+pub const APLT_CTL: &str = "alfad-ctl";
+
+// Compilation applet
+pub const APLT_COMPILE: &str = "alfad-compile";
+
+// The /sbin/init
+pub const APLT_INIT: &str = "init";
+
+/// Sockets
+pub const DIR_RUN: &str = "/run/var";
+
+/// Configuration directory
+pub const DIR_CFG: &str = "/etc/alfad";
+
+/// Configuration bytecode
+pub const FILE_CFG_BT: &str = "alfad.d.cache";
